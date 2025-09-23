340b000c310a628fc463a0e10676c83c6f5b6e76
Kryllee <k.laderaaa@gmail.com>
2025-09-24 00:02:38 +0800

    Search draft

--- mobile/app/(tabs)/_layout.jsx
+++ mobile/app/(tabs)/_layout.jsx
@@ -1,5 +1,6 @@
 import { Ionicons } from "@expo/vector-icons"
 import { Tabs } from "expo-router";
+import { Pressable } from "react-native";
 
 export default function TabLayout() {
   return (
--- mobile/app/(tabs)/search.jsx
+++ mobile/app/(tabs)/search.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, } from 'react-native'
 import React from 'react'
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const search = () => {
   return (
     <View style={styles.container1}>
       <View style={styles.container2}>
+        {/*Image*/}
+        <View style={styles.searchBar}>
+         <Text style={{ fontStyle: 'italic', position: "absolute", left: 35, fontSize: 16, top: 10 }}>Search for ideas</Text>
+            <Ionicons name="search" size={21} color="black"  style={{ position: "absolute", left: 10, top: 10 }} />
+            <Ionicons name="camera-outline" size={22} color="black" style={{ position: "absolute", right: 12 , top: 10}} />
+        </View>
       </View>
     </View>
   )
@@ -14,12 +21,17 @@ const styles = {
   container1: {
     flex: 1, 
     paddingTop: 40,
-    backgroundColor: '#ad3535ff',
+    backgroundColor: '#fff',
   },
   container2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: 300,
+    backgroundColor: '#d9d9d9',
+  },
+  searchBar: {
+    height: 45,
+    margin: 10,
+    backgroundColor: '#fff',
+    borderRadius: 8,
   },
 }
 
